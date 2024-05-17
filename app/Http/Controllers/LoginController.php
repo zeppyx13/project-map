d94ae7ed3d420a457d6bb5251dc445cca2b34485
Gung Nanda <gungnands@gmail.com>
2024-05-17 17:43:02 +0800

    test brach

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -28,7 +28,6 @@ class LoginController extends Controller
                 $request->session()->regenerate();
                 return redirect()->intended('/dashboard');
             }
-
             throw new \Exception('Invalid login credentials.'); // Throw specific exception
         } catch (\Exception $e) {
             // Handle authentication error
